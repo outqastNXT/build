26e64edb2feca319f77d6942fb8ff4cfe9a869e0
deadman96385 <seanhoyt963@gmail.com>
2014-12-31 19:44:19 -0500

    Kill some log spam
    Thanks to @Cl3Kener for taking lots of his precious time to help me find these things that were driving me up the wall.  I should have authored this commit as him but I didn't.  Sorry buddy maybe next time....
    
    Change-Id: I8d6cd504afe83954964f71eb7d8f802624d2e595
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/raw_executable.mk

--- core/raw_executable.mk
+++ core/raw_executable.mk
@@ -13,8 +13,8 @@ $(all_objects) : PRIVATE_TARGET_GLOBAL_CFLAGS :=
 $(all_objects) : PRIVATE_TARGET_GLOBAL_CPPFLAGS :=
 
 $(LOCAL_BUILT_MODULE): $(all_objects) $(all_libraries)
-	@$(mkdir -p $(dir $@)
-	@echo "target Linking: $(PRIVATE_MODULE)"
+	$(hide) $(mkdir -p $(dir $@)
+	@echo -e ${CL_GRN}"target Linking:"${CL_RST}" $(PRIVATE_MODULE)"
 	$(hide) $(TARGET_LD) \
 		$(addprefix --script ,$(PRIVATE_LINK_SCRIPT)) \
 		$(PRIVATE_RAW_EXECUTABLE_LDFLAGS) \
